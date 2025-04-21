65fa379fc5c497ec84812f8c8d01ea47faf88c9e
Hemant Karekar <hemant.v.karekar@gmail.com>
2025-04-21 13:24:51 +0530

    About Us Change

--- website/resources/views/Frontend/AboutUs.blade.php
+++ website/resources/views/Frontend/AboutUs.blade.php
@@ -12,7 +12,7 @@
         background-color: #F5F5F5;
     }
 
- 
+
 </style>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.20.0/jquery.validate.min.js"
     integrity="sha512-WMEKGZ7L5LWgaPeJtw9MBM4i5w5OSBlSjTjCtSnvFJGSVD26gE5+Td12qN5pvWXhuWaWcVwF++F7aqu9cvqP0A=="
@@ -61,6 +61,69 @@
             </div>
         </div>
     </div>
+
+    <section class="position-relative overflow-hidden space overflow-hidden" id="process-sec">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8">
+                    <div class="title-area text-center">
+                        <div class="shadow-title">Process</div>
+                        <span class="sub-title sub-title4 mb-4 mb-md-5">Work Process<span></span></span>
+                        <h2 class="sec-title  ">The work process of our agency</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="step-wrap">
+                <div class="process-card_wrapp">
+                    <div class="process-card">
+                        <div class="box-wrapp">
+                            <div class="box-icon">
+                                <img src="{{ asset('frontend-assets/img/icon/process_1_1.svg') }}" alt="icon">
+                            </div>
+                            <div class="box-shape"><img src="{{ asset('frontend-assets/img/icon/arrow.png') }}" alt=""></div>
+                            <div class="box-number">
+                                01 </div>
+                        </div>
+                        <div class="box-content">
+                            <h2 class="box-title">Research and Strategy</h2>
+                            <p class="box-text">Conduct thorough market of the research to the fast target audience behaviors. Submit as many design tasks</p>
+
+                        </div>
+                    </div>
+                    <div class="process-card">
+                        <div class="box-wrapp">
+                            <div class="box-icon">
+                                <img src="{{ asset('frontend-assets/img/icon/process_1_2.svg') }}" alt="icon">
+                            </div>
+                            <div class="box-shape"><img src="{{ asset('frontend-assets/img/icon/arrow.png') }}" alt=""></div>
+                            <div class="box-number">
+                                02 </div>
+                        </div>
+                        <div class="box-content">
+                            <h2 class="box-title">Plan Customization</h2>
+                            <p class="box-text">Conduct thorough market of the research to the fast target audience behaviors. Submit as many design tasks</p>
+
+                        </div>
+                    </div>
+                    <div class="process-card">
+                        <div class="box-wrapp">
+                            <div class="box-icon">
+                                <img src="{{ asset('frontend-assets/img/icon/process_1_3.svg') }}" alt="icon">
+                            </div>
+                            <div class="box-shape"><img src="{{ asset('frontend-assets/img/icon/arrow.png') }}" alt=""></div>
+                            <div class="box-number">
+                                03 </div>
+                        </div>
+                        <div class="box-content">
+                            <h2 class="box-title">Finished &amp; User Testing</h2>
+                            <p class="box-text">Conduct thorough market of the research to the fast target audience behaviors. Submit as many design tasks</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
     <div class="overflow-hidden mb-md-3 mt-md-3 mt-0 md-0" id="about-process">
         <div class="container process-container text-center">
