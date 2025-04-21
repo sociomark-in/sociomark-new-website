8793921ce71b55c9a6a961f1bdc47ff15f37fa55
Hemant Karekar <hemant.v.karekar@gmail.com>
2025-04-21 13:36:17 +0530

    Our Work

--- website/resources/views/Frontend/AboutUs.blade.php
+++ website/resources/views/Frontend/AboutUs.blade.php
@@ -128,69 +128,6 @@
         </div>
     </section>
 
-    <section class="position-relative overflow-hidden space overflow-hidden" id="process-sec">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-8">
-                    <div class="title-area text-center">
-                        <div class="shadow-title">Process</div>
-                        <span class="sub-title sub-title4 mb-4 mb-md-5">Work Process<span></span></span>
-                        <h2 class="sec-title  ">The work process of our agency</h2>
-                    </div>
-                </div>
-            </div>
-            <div class="step-wrap">
-                <div class="process-card_wrapp">
-                    <div class="process-card">
-                        <div class="box-wrapp">
-                            <div class="box-icon">
-                                <img src="{{ asset('frontend-assets/img/icon/process_1_1.svg') }}" alt="icon">
-                            </div>
-                            <div class="box-shape"><img src="{{ asset('frontend-assets/img/icon/arrow.png') }}" alt=""></div>
-                            <div class="box-number">
-                                01 </div>
-                        </div>
-                        <div class="box-content">
-                            <h2 class="box-title">Research and Strategy</h2>
-                            <p class="box-text">Conduct thorough market of the research to the fast target audience behaviors. Submit as many design tasks</p>
-
-                        </div>
-                    </div>
-                    <div class="process-card">
-                        <div class="box-wrapp">
-                            <div class="box-icon">
-                                <img src="{{ asset('frontend-assets/img/icon/process_1_2.svg') }}" alt="icon">
-                            </div>
-                            <div class="box-shape"><img src="{{ asset('frontend-assets/img/icon/arrow.png') }}" alt=""></div>
-                            <div class="box-number">
-                                02 </div>
-                        </div>
-                        <div class="box-content">
-                            <h2 class="box-title">Plan Customization</h2>
-                            <p class="box-text">Conduct thorough market of the research to the fast target audience behaviors. Submit as many design tasks</p>
-
-                        </div>
-                    </div>
-                    <div class="process-card">
-                        <div class="box-wrapp">
-                            <div class="box-icon">
-                                <img src="{{ asset('frontend-assets/img/icon/process_1_3.svg') }}" alt="icon">
-                            </div>
-                            <div class="box-shape"><img src="{{ asset('frontend-assets/img/icon/arrow.png') }}" alt=""></div>
-                            <div class="box-number">
-                                03 </div>
-                        </div>
-                        <div class="box-content">
-                            <h2 class="box-title">Finished &amp; User Testing</h2>
-                            <p class="box-text">Conduct thorough market of the research to the fast target audience behaviors. Submit as many design tasks</p>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
     <div class="overflow-hidden mb-md-3 mt-md-3 mt-0 md-0 " id="about-process">
         <div class="container process-container text-center">
             <h2 class="fw-bold mb-5 sec-title"
@@ -279,7 +216,7 @@
         </div>
     </div>
 
-    <div class="ourwork overflow-hidden space " id="aboutUs-KEyPoints">
+    <section class="ourwork overflow-hidden space " id="aboutUs-KEyPoints">
         <div class="container">
             <div class="text-center content-box">
                 <!-- <h2 class="sec-title">Making a mark, one day at a time!</h2> -->
@@ -379,7 +316,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 
     <section class="about-area overflow-hidden position-relative p-0" id="about-banner" style="background-color: black;">
         <div class="container-fluid">
