0e3f014ba09ed39fd5d2435fb04536c23ab392ec
shrutipatil-19 <shrupatil1319@gmail.com>
2025-04-23 14:05:35 +0530

    rename industry

--- website/resources/views/Frontend/Industry/education.blade.php
+++ website/resources/views/Frontend/Industry/education.blade.php
@@ -141,7 +141,7 @@
             </div>
         </div>
     </div>
-    @include('frontend/partial/clientList')
+    @include('Frontend/partial/clientList')
   
     <div class="overflow-hidden space" id="about-sec4">
         <div class="container">
