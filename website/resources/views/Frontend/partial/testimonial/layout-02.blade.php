ee7774be1c7f397fcdd82ad07a6878997d21ac3c
Hemant Karekar <hemant.v.karekar@gmail.com>
2025-04-30 12:20:29 +0530

    Testimonials

--- website/resources/views/Frontend/partial/testimonial/layout-02.blade.php
+++ website/resources/views/Frontend/partial/testimonial/layout-02.blade.php
@@ -6,77 +6,147 @@
             <h2 class="sec-title  ">What <span class="text-blue">Our Clients Say</span> About Us?</h2>
         </div>
         <div class="slider-area testi-box-area">
-            <div class="swiper th-slider has-shadow testiSlider2 swiper-fade swiper-initialized swiper-horizontal swiper-watch-progress swiper-backface-hidden"
-                id="testiSlide2"
-                data-slider-options="{&quot;effect&quot;:&quot;fade&quot;,&quot;loop&quot;:true,&quot;breakpoints&quot;:{&quot;0&quot;:{&quot;slidesPerView&quot;:1},&quot;576&quot;:{&quot;slidesPerView&quot;:&quot;1&quot;},&quot;768&quot;:{&quot;slidesPerView&quot;:&quot;1&quot;},&quot;992&quot;:{&quot;slidesPerView&quot;:&quot;1&quot;},&quot;1200&quot;:{&quot;slidesPerView&quot;:&quot;1&quot;}}}">
+            <div class="swiper th-slider" id="testiSlide2"
+                data-slider-options="{effect:fade,loop:true,breakpoints:{0:{slidesPerView:1},576:{slidesPerView:1},768:{slidesPerView:1},992:{slidesPerView:1},1200:{slidesPerView:1}}}">
                 <div class="swiper-wrapper" id="swiper-wrapper-067831845871f9ee" aria-live="off"
                     style="transition-duration: 0ms; transition-delay: 0ms;">
-                    <div class="swiper-slide swiper-slide-next" role="group" aria-label="2 / 3"
-                        data-swiper-slide-index="1"
-                        style="width: 1290px; opacity: 1; transform: translate3d(0px, 0px, 0px); transition-duration: 0ms;">
+                    <div class="swiper-slide">
                         <div class="testi-grid">
                             <div class="box-content">
                                 <div class="testi-logo">
-                                    <img src="https://placehold.co/200x80" alt="">
+                                    <img src="{{ asset('frontend-assets/img/clients/times.jpg') }}" alt="project image"
+                                        style="height: 80px">
                                 </div>
-                                <h3 class="box-title2">Kornix is the best digital agency i have ever seen! Highly
-                                    Recommended!</h3>
-                                <p class="box-text">If you have specific questions about gathering or analyzing
-                                    customer feedback, or if you're looking for general information on the
-                                    importance of customer feedback, feel free to ask. Additionally, if you have a
-                                    sample of customer feedback that you'd like assistance with, you can share it,
-                                    and I can provide insights or suggestions based on you provide</p>
-                                <h3 class="box-title">Abraham Khalil</h3>
-                                <span class="box-desig">UI/UX Designer</span>
-                            </div>
-                            <div class="box-img">
-                                <img src="https://placehold.co/200x200" class="w-100" alt="image">
+                                <h3 class="box-title2">"Seamless Call for Entries Thanks to Sociomark!"</h3>
+                                <p class="box-text">
+                                    Sociomark, particularly Heta, Vivek, and Hemant, is highly appreciated for their
+                                    excellent work in developing and managing the Leaders of Tomorrow website. Their
+                                    efforts ensured a smooth and efficient awards process, especially in managing the
+                                    Call for Entries, which was previously problematic. Hemant is specifically
+                                    acknowledged for his problem-solving skills and timely execution. Heta and Vivek are
+                                    thanked for their leadership and support. Overall, the collaboration was a success,
+                                    and there's anticipation for future partnerships.
+                                </p>
+                                <p class="box-text">
+                                    Thank you once again for your outstanding contributions! We look forward to working
+                                    with you again in the future.
+                                </p>
+                                <h3 class="box-title">Shubhankar Jadhav</h3>
+                                <span class="box-desig">Manager, Times Network</span>
                             </div>
                         </div>
                     </div>
-                    <div class="swiper-slide swiper-slide-prev" role="group" aria-label="3 / 3"
-                        data-swiper-slide-index="2"
-                        style="width: 1290px; opacity: 1; transform: translate3d(-1290px, 0px, 0px); transition-duration: 0ms;">
+                    <div class="swiper-slide">
                         <div class="testi-grid">
                             <div class="box-content">
                                 <div class="testi-logo">
-                                    <img src="https://placehold.co/200x80" alt="">
+                                    <img src="{{ asset('frontend-assets/img/clients/ayushakti.jpg') }}"
+                                        style="height: 80px" alt="project image">
                                 </div>
-                                <h3 class="box-title2">Kornix is the best digital agency i have ever seen! Highly
-                                    Recommended!</h3>
-                                <p class="box-text">It sounds like you're expressing positive feedback or a
-                                    statement about a product or service being loved by customers worldwide. If this
-                                    is a statement you want to share or discuss, it's great to highlight positive
-                                    feedback! Customer satisfaction and positive testimonials can be powerful assets
-                                    for a product or services businesses I'm here to help!</p>
-                                <h3 class="box-title">Jenny Wilson</h3>
-                                <span class="box-desig">Project Manager</span>
-                            </div>
-                            <div class="box-img">
-                                <img src="https://placehold.co/200x200" class="w-100" alt="image">
+                                <h3 class="box-title2">"Sociomark is literally our partner in Digital Marketing"</h3>
+                                <p class="box-text">
+                                    Ever since we started working with Sociomark, <strong>our return on
+                                        investment on marketing have significantly improved, almost
+                                        doubled</strong>.
+                                </p>
+                                <div class="d-none d-md-block">
+                                    <p class="box-text">
+                                        They help us in creating a robust marketing strategy. They also
+                                        create those creatives which raise people's eyeballs when they watch
+                                        it on social media and the likes are only increasing month and month
+                                        because of that. Sociomark is so committed that they always deliver
+                                        what they promise each month. I am extremely happy working with
+                                        them. <strong> Best of luck Sociomark! </strong>
+                                    </p>
+                                </div>
+                                <h3 class="box-title">Dr. Smita Naram</h3>
+                                <span class="box-desig">Founder, Ayushakti Ayurveda</span>
                             </div>
                         </div>
                     </div>
-                    <div class="swiper-slide swiper-slide-visible swiper-slide-active" role="group"
-                        aria-label="1 / 3" data-swiper-slide-index="0"
-                        style="width: 1290px; opacity: 1; transform: translate3d(-2580px, 0px, 0px); transition-duration: 0ms;">
+                    <div class="swiper-slide">
                         <div class="testi-grid">
                             <div class="box-content">
                                 <div class="testi-logo">
-                                    <img src="https://placehold.co/200x80" alt="">
+                                    <img src="{{ asset('frontend-assets/img/clients/worldgrad.jpg') }}"
+                                        alt="project image" style="height: 80px">
                                 </div>
-                                <h3 class="box-title2">Kornix is the best digital agency i have ever seen! Highly
-                                    Recommended!</h3>
-                                <p class="box-text">We can work with your existing website if it's technically
-                                    sound and meets your needs. If necessary, we can also recommend and implement
-                                    improvements or a complete redesign to enhance performance and user experience.
-                                    We start with a comprehensive analysis of your business, industry, target
-                                    audience, and current digital presence.</p>
-                                <h3 class="box-title">Diana Loreza</h3>
-                                <span class="box-desig">Director of Gymstory</span>
+                                <h3 class="box-title2">"Sociomark's Exceptional Ability to Capture and Visualize Brand
+                                    Voice"</h3>
+                                <p class="box-text">Working with Sociomark has been an absolute pleasure. They have
+                                    consistently exceeded my expectations in handling The WorldGrad's social media
+                                    creative needs, from stunning static images to engaging reels and videos. Their
+                                    creativity and attention to detail have truly brought our brand's vision to life
+                                    across all platforms.</p>
+                                <p class="box-text">We highly recommend Sociomark to anyone in need of top-notch
+                                    creative services!</p>
+                                <h3 class="box-title">Suyash Birje</h3>
+                                <span class="box-desig">The WorldGrad</span>
                             </div>
-                            <div class="box-img">
-                                <img src="https://placehold.co/200x200" class="w-100" alt="image">
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="testi-grid">
+                            <div class="box-content">
+                                <div class="testi-logo">
+                                    <img src="{{ asset('frontend-assets/img/clients/network18.jpg') }}"
+                                        alt="project image" style="height: 80px">
+                                </div>
+                                <h3 class="box-title2">"A Rewarding 3-Year Journey with the Team at SocioMark"</h3>
+                                <p class="box-text">
+                                    Working with SocioMark for about 3 years now. Lovely people to
+                                    work with. They have hardly ever missed any deadlines.The best part about them
+                                    is that they no longer feel like an external agency but someone who
+                                    is a part of Network18 group.We know whenever we are in trouble or
+                                    that late night call for an urgent creative SocioMark will always be
+                                    there.They have set benchmarks for others by their creative work
+                                    done for the Road to safety campaign.
+                                </p>
+                                <h3 class="box-title">Apurva Shah</h3>
+                                <span class="box-desig">AGM, Marketing, Network18</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="testi-grid">
+                            <div class="box-content">
+                                <div class="testi-logo">
+                                    <img src="{{ asset('frontend-assets/img/clients/tpc01.jpg') }}"
+                                        alt="project image" style="height: 80px">
+                                </div>
+                                <h3 class="box-title2">"The Sociomark team is professional, flexible, and committed to their work"</h3>
+                                <p class="box-text">
+                                        Sociomark team is absolutely very professional in their work yet
+                                        bring in high flexibility in terms of ownership in delivery.
+                                        <br>
+                                        All the tasks and works are delivered with utmost commitment and
+                                        dedication and on time. they never said no and always valued and
+                                        worked on our feedbacks we have had a long term engagement with them
+                                        and they are an absolute pleasure to work with!
+                                </p>
+                                <h3 class="box-title">Siju Thomas</h3>
+                                <span class="box-desig">CEO, The Professional Couriers</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="testi-grid">
+                            <div class="box-content">
+                                <div class="testi-logo">
+                                    <img src="{{ asset('frontend-assets/img/clients/network18.jpg') }}"
+                                        alt="project image" style="height: 80px">
+                                </div>
+                                <h3 class="box-title2">"Effective Facebook & Instagram Marketing by Sociomark for GrillBox"</h3>
+                                <p class="box-text">
+                                    Its a pleasure working with Sociomark for social media marketing of
+                                    our brand GrillBox through Facebook and Instagram.
+                                    <br>
+                                    Sociomark helped us to reach many users through their creative
+                                    contents. They were easy to work with and were flexible with meeting
+                                    our needs.
+                                </p>
+                                <h3 class="box-title">Shafeeq Mareen</h3>
+                                <span class="box-desig">Founder, GrillBox</span>
                             </div>
                         </div>
                     </div>
@@ -88,5 +158,16 @@
             <button data-slider-next="#testiSlide2" class="slider-arrow style3 slider-next"><i
                     class="fas fa-arrow-right"></i></button>
         </div>
+        @push('scripts')
+            <script>
+                new Swiper('.testi-box-area #testiSlide2', {
+                    loop: true,
+                    navigation: {
+                        nextEl: ".testi-box-area .slider-next",
+                        prevEl: ".testi-box-area .slider-prev",
+                    },
+                })
+            </script>
+        @endpush
     </div>
 </section>
