5c0001cdb58a5b075a0cd7e70a27702cb36b71eb
Hemant Karekar <hemant.v.karekar@gmail.com>
2025-05-28 16:17:57 +0530

    Defer Remove

--- website/resources/views/Frontend/partial/jsLinks.blade.php
+++ website/resources/views/Frontend/partial/jsLinks.blade.php
@@ -4,41 +4,41 @@
  <!-- Swiper Slider -->
  <script src="{{ asset('frontend-assets/js/swiper-bundle.min.js') }}"></script>
  <!-- Bootstrap -->
- <script defer src="{{ asset('frontend-assets/js/bootstrap.min.js') }}"></script>
+ <script src="{{ asset('frontend-assets/js/bootstrap.min.js') }}"></script>
  <!-- Magnific Popup -->
- <script defer src="{{ asset('frontend-assets/js/jquery.magnific-popup.min.js') }}"></script>
+ <script src="{{ asset('frontend-assets/js/jquery.magnific-popup.min.js') }}"></script>
  <!-- Counter Up -->
- <script defer src="{{ asset('frontend-assets/js/jquery.counterup.min.js') }}"></script>
+ <script src="{{ asset('frontend-assets/js/jquery.counterup.min.js') }}"></script>
  <!-- Circle Progress -->
- <script defer src="{{ asset('frontend-assets/js/circle-progress.js') }}"></script>
+ <script src="{{ asset('frontend-assets/js/circle-progress.js') }}"></script>
  <!-- Range Slider -->
- <script defer src="{{ asset('frontend-assets/js/jquery-ui.min.js') }}"></script>
+ <script src="{{ asset('frontend-assets/js/jquery-ui.min.js') }}"></script>
  <!-- Imagesloadedr -->
- <script defer src="{{ asset('frontend-assets/js/imagesloaded.pkgd.min.js') }}"></script>
+ <script src="{{ asset('frontend-assets/js/imagesloaded.pkgd.min.js') }}"></script>
  <!-- isotope -->
- <script defer src="{{ asset('frontend-assets/js/isotope.pkgd.min.js') }}"></script>
+ <script src="{{ asset('frontend-assets/js/isotope.pkgd.min.js') }}"></script>
  <!-- Tilt.jquery -->
- <script defer src="{{ asset('frontend-assets/js/tilt.jquery.min.js') }}"></script>
+ <script src="{{ asset('frontend-assets/js/tilt.jquery.min.js') }}"></script>
  <!-- Nice-select -->
- <script defer src="{{ asset('frontend-assets/js/nice-select.min.js') }}"></script>
+ <script src="{{ asset('frontend-assets/js/nice-select.min.js') }}"></script>
  <!-- wow -->
- <script defer src="{{ asset('frontend-assets/js/wow.min.js') }}"></script>
+ <script src="{{ asset('frontend-assets/js/wow.min.js') }}"></script>
  <!-- Particles JS -->
- <script defer src="{{ asset('frontend-assets/js/particles.min.js') }}"></script>
+ <script src="{{ asset('frontend-assets/js/particles.min.js') }}"></script>
 
 
- <script defer src="{{ asset('frontend-assets/js/particles-config.js') }}"></script>
+ <script src="{{ asset('frontend-assets/js/particles-config.js') }}"></script>
 
  <!-- Gsap JS -->
  {{-- <script src="{{ asset('frontend-assets/js/gsap.min.js') }}"></script>
     <script src="{{ asset('frontend-assets/js/ScrollTrigger.min.js') }}"></script> --}}
- <script defer src="https://cdn.jsdelivr.net/npm/gsap@3.12.7/dist/gsap.min.js"></script>
- <script defer src="https://cdn.jsdelivr.net/npm/gsap@3.12.7/dist/ScrollTrigger.min.js"></script>
- <script defer src="{{ asset('frontend-assets/js/Splitetext.js') }}"></script>
- <script defer src="{{ asset('frontend-assets/js/lenis.min.js') }}"></script>
+ <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.7/dist/gsap.min.js"></script>
+ <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.7/dist/ScrollTrigger.min.js"></script>
+ <script src="{{ asset('frontend-assets/js/Splitetext.js') }}"></script>
+ <script src="{{ asset('frontend-assets/js/lenis.min.js') }}"></script>
 
  <!-- imageRevealHover JS -->
- <script defer src="{{ asset('frontend-assets/js/imageRevealHover.js') }}"></script>
+ <script src="{{ asset('frontend-assets/js/imageRevealHover.js') }}"></script>
 
  <!-- Main Js File -->
- <script defer src="{{ asset('frontend-assets/js/main.js') }}"></script>
+ <script src="{{ asset('frontend-assets/js/main.js') }}"></script>
